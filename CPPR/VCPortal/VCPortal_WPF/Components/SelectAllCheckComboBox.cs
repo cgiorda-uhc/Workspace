09ac8fa897bfa4d6245d4bcf15964a4b22339013
Chris Giordan <chris_giordano@uhc.com>
2023-05-24 16:38:02 -0400

    d

---
+++ CPPR/VCPortal/VCPortal_WPF/Components/SelectAllCheckComboBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.Toolkit;
+
+namespace VCPortal_WPF.Components;
+public class SelectAllCheckComboBox : CheckComboBox
+{
+    protected override void UpdateText()
+    {
+        //Do not display the “Select All” in the TextBox.
+        var selectedItemsList = this.SelectedItems.Cast<object>().Select(x => this.GetItemDisplayValue(x)).Where(x => !x.Equals("--All--"));
+
+        var newValue = String.Join(this.Delimiter, selectedItemsList);
+        if (String.IsNullOrEmpty(this.Text) || !this.Text.Equals(newValue))
+        {
+            this.SetCurrentValue(CheckComboBox.TextProperty, newValue);
+        }
+    }
+}
