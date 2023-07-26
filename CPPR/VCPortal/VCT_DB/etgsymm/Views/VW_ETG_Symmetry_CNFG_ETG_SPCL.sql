42b5b3ae39c7f2d4a53247402a648335047c4cf4
Chris Giordan <chris_giordano@uhc.com>
2023-07-26 16:03:07 -0400

    f

--- CPPR/VCPortal/VCT_DB/etgsymm/Views/VW_ETG_Symmetry_CNFG_ETG_SPCL.sql
+++ CPPR/VCPortal/VCT_DB/etgsymm/Views/VW_ETG_Symmetry_CNFG_ETG_SPCL.sql
@@ -2,6 +2,7 @@
 	AS 
 	
 	
+		
 	SELECT 
 
 
@@ -22,4 +23,60 @@ LEFT OUTER JOIN vct.ETG_Dim_Premium_Spec_Master AS p ON f.Premium_Specialty_id =
  f.PD_Version = (SELECT max(PD_Version) FROM etgsymm.ETG_Fact_Symmetry)
  AND  f.ETG_Base_Class <> 000000
 AND f.Premium_Specialty_Id IS NOT NULL
-AND f.ETG_Fact_Symmetry_Id IN  (SELECT ETG_Fact_Symmetry_Id FROM [etgsymm].[VW_ETG_Summary_Final] WHERE [UGAP_Changes] <> 'Not Mapped' AND EC_Current_Mapping = 'Mapped')
+AND f.ETG_Fact_Symmetry_Id IN  (SELECT ETG_Fact_Symmetry_Id FROM [etgsymm].[VW_ETG_Summary_Final] WHERE  EC_Current_Mapping = 'Mapped')
+AND f.[EC_Treatment_Indicator] IS NOT NULL
+
+UNION ALL
+
+
+	
+	SELECT 
+
+
+NULL as CNFG_ETG_SPCL_SYS_ID,
+
+  f.[ETG_Base_Class] AS [ETG_BASE_CLASS],
+ 0 as TRT_CD,
+
+
+CAST(f.PD_Version AS DECIMAL(10,2))  as PREM_DESG_VER_NBR,
+p.Premium_Specialty as PREM_SPCL_CD,
+ 
+NULL as [NOTES]
+	
+FROM  etgsymm.ETG_Fact_Symmetry AS f 
+LEFT OUTER JOIN vct.ETG_Dim_Premium_Spec_Master AS p ON f.Premium_Specialty_id = p.Premium_Specialty_id
+ WHERE 
+ f.PD_Version = (SELECT max(PD_Version) FROM etgsymm.ETG_Fact_Symmetry)
+ AND  f.ETG_Base_Class <> 000000
+AND f.Premium_Specialty_Id IS NOT NULL
+AND f.ETG_Fact_Symmetry_Id IN  (SELECT ETG_Fact_Symmetry_Id FROM [etgsymm].[VW_ETG_Summary_Final] WHERE  EC_Current_Mapping = 'Mapped')
+AND f.[EC_Treatment_Indicator] IS NULL
+
+
+UNION ALL
+
+
+	
+	SELECT 
+
+
+NULL as CNFG_ETG_SPCL_SYS_ID,
+
+  f.[ETG_Base_Class] AS [ETG_BASE_CLASS],
+ 1 as TRT_CD,
+
+
+CAST(f.PD_Version AS DECIMAL(10,2))  as PREM_DESG_VER_NBR,
+p.Premium_Specialty as PREM_SPCL_CD,
+ 
+NULL as [NOTES]
+	
+FROM  etgsymm.ETG_Fact_Symmetry AS f 
+LEFT OUTER JOIN vct.ETG_Dim_Premium_Spec_Master AS p ON f.Premium_Specialty_id = p.Premium_Specialty_id
+ WHERE 
+ f.PD_Version = (SELECT max(PD_Version) FROM etgsymm.ETG_Fact_Symmetry)
+ AND  f.ETG_Base_Class <> 000000
+AND f.Premium_Specialty_Id IS NOT NULL
+AND f.ETG_Fact_Symmetry_Id IN  (SELECT ETG_Fact_Symmetry_Id FROM [etgsymm].[VW_ETG_Summary_Final] WHERE  EC_Current_Mapping = 'Mapped')
+AND f.[EC_Treatment_Indicator] IS NULL
