74e9dcaeed29264d4db40bd5fbd76873e8575044
Chris Giordan <chris_giordano@uhc.com>
2023-10-16 11:00:48 -0400

    d

---
+++ CPPR/VCPortal/VCPortal_Models/Models/EDCAdhoc/EDC_CS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCPortal_Models.Models.EDCAdhoc;
+public class EDC_CS
+{
+    public Int64 MPIN { get; set; }
+    public string IRS_TAX_ID { get; set; }
+    public string CLCL_ID { get; set; }
+    public Int64 CDML_SEQ_NO { get; set; }
+    public string CLCL_CUR_STS { get; set; }
+    public string Proc_Cd { get; set; }
+    public string PROC_DESC { get; set; }
+    public string smartmodelcpt { get; set; }
+    public string REVCODE { get; set; }
+    public string CLCL_PA_ACCT_NO { get; set; }
+    public int AGE { get; set; }
+    public string GENDER { get; set; }
+    public DateTime service_date { get; set; }
+    public DateTime paid_date { get; set; }
+    public string erlvl { get; set; }
+    public string diag1 { get; set; }
+    public string diag1desc { get; set; }
+    public string diag2 { get; set; }
+    public string diag2desc { get; set; }
+    public string diag3 { get; set; }
+    public string diag3desc { get; set; }
+    public string diag4 { get; set; }
+    public string diag4desc { get; set; }
+    public string diag5 { get; set; }
+    public string diag5desc { get; set; }
+    public string diag6 { get; set; }
+    public string diag6desc { get; set; }
+    public string diag7 { get; set; }
+    public string diag7desc { get; set; }
+    public string diag8 { get; set; }
+    public string diag8desc { get; set; }
+    public string diag9 { get; set; }
+    public string diag9desc { get; set; }
+    public string diag10 { get; set; }
+    public string diag10desc { get; set; }
+    public string diag11 { get; set; }
+    public string diag11desc { get; set; }
+    public string diag12 { get; set; }
+    public string diag12desc { get; set; }
+    public string diag13 { get; set; }
+    public string diag13desc { get; set; }
+    public string diag14 { get; set; }
+    public string diag14desc { get; set; }
+    public string diag15 { get; set; }
+    public string diag15desc { get; set; }
+    public string ReasForVisit1 { get; set; }
+    public string rfvdesc1 { get; set; }
+    public string ReasForVisit2 { get; set; }
+    public string rfvdesc2 { get; set; }
+    public string ECC_Code1 { get; set; }
+    public string ecc_desc { get; set; }
+    public string ECC_Code2 { get; set; }
+    public string ecc2_desc { get; set; }
+
+}
---
+++ CPPR/VCPortal/VCPortal_Models/Models/EDCAdhoc/EDC_MR.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCPortal_Models.Models.EDCAdhoc;
+public class EDC_MR
+{
+    public string CLM_ID { get; set; }
+    public string PROV_TIN { get; set; }
+    public Int64 MPIN { get; set; }
+    public string SRVC_PROV_NM { get; set; }
+    public DateTime FST_SRVC_DT { get; set; }
+    public string PROC_CD { get; set; }
+    public string PROC_DESC { get; set; }
+    public string ModelCPT { get; set; }
+    public string RVNU_CD { get; set; }
+    public string MBR_ID { get; set; }
+    public string PTNT_ACCT_NBR { get; set; }
+    public int MBR_AGE { get; set; }
+    public string GDR_CD { get; set; }
+    public DateTime BTH_DT { get; set; }
+    public string PTNT_RSN_FOR_VST_1_CD { get; set; }
+    public string rfv1_desc { get; set; }
+    public string PTNT_RSN_FOR_VST_2_CD { get; set; }
+    public string rfv2_desc { get; set; }
+    public string PTNT_RSN_FOR_VST_3_CD { get; set; }
+    public string rfv3_desc { get; set; }
+    public string DIAG_1_CD { get; set; }
+    public string diag1desc { get; set; }
+    public string DIAG_2_CD { get; set; }
+    public string diag2desc { get; set; }
+    public string DIAG_3_CD { get; set; }
+    public string diag3desc { get; set; }
+    public string DIAG_4_CD { get; set; }
+    public string diag4desc { get; set; }
+    public string DIAG_5_CD { get; set; }
+    public string diag5desc { get; set; }
+    public string DIAG_6_CD { get; set; }
+    public string diag6desc { get; set; }
+    public string DIAG_7_CD { get; set; }
+    public string diag7desc { get; set; }
+    public string DIAG_8_CD { get; set; }
+    public string diag8desc { get; set; }
+    public string DIAG_9_CD { get; set; }
+    public string diag9desc { get; set; }
+    public string DIAG_10_CD { get; set; }
+    public string diag10desc { get; set; }
+    public string DIAG_11_CD { get; set; }
+    public string diag11desc { get; set; }
+    public string EXTR_CAUS_CD { get; set; }
+    public string ecc_desc { get; set; }
+    public int DOS_YEAR { get; set; }
+    public int DOS_MNTH { get; set; }
+    public int PAID_YEAR { get; set; }
+    public int PAID_MNTH { get; set; }
+
+}
